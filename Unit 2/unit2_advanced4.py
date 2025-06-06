04ce64b980fe145adc95411c38b20f5cac9c0d90
mives2 <mives458@gmail.com>
2025-06-05 21:26:37 -0500

    Bug Fix in Unit 2 Prob 4

--- Unit 2/unit2_advanced4.py
+++ Unit 2/unit2_advanced4.py
@@ -14,7 +14,7 @@ def is_balanced(code):
 
     for char in code:
         if char not in my_dict:
-            my_dict[char] = 0
+            my_dict[char] = 1
         else:
             my_dict[char] += 1
 
